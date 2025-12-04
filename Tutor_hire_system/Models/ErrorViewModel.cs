50760377bda7812d0400d00ff937b072d044982e
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-04 18:38:37 +0200

    Add project files.

---
+++ Tutor_hire_system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutor_hire_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
