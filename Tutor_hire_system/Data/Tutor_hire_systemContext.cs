b7bc6269996ec8aa80cd2a41ea61e27e97eadf2c
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-06 20:07:12 +0200

    Added constraits to the context

--- Tutor_hire_system/Data/Tutor_hire_systemContext.cs
+++ Tutor_hire_system/Data/Tutor_hire_systemContext.cs
@@ -20,5 +20,55 @@ namespace Tutor_hire_system.Data
         public DbSet<Tutor_hire_system.Models.Post> Post { get; set; } = default!;
         public DbSet<Tutor_hire_system.Models.Job> Job { get; set; } = default!;
         public DbSet<Tutor_hire_system.Models.Role> Role { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ROLE 1-M USER
+            modelBuilder.Entity<Role>()
+                .HasMany(r => r.Users)
+                .WithOne(u => u.Role)
+                .HasForeignKey(u => u.RoleId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // USER 1-1 STUDENT
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Student)
+                .WithOne(s => s.User)
+                .HasForeignKey<Student>(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // USER 1-1 TUTOR
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Tutor)
+                .WithOne(t => t.User)
+                .HasForeignKey<Tutor>(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // STUDENT 1-M POST
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Student)
+                .WithMany(s => s.Posts)
+                .HasForeignKey(p => p.StudentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // POST 0-1 JOB
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Job)
+                .WithOne(j => j.Post)
+                .HasForeignKey<Job>(j => j.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // TUTOR 1-M JOB
+            modelBuilder.Entity<Job>()
+                .HasOne(j => j.Tutor)
+                .WithMany(t => t.Jobs)
+                .HasForeignKey(j => j.TutorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // UNIQUE CONSTRAINT: A Post can only have ONE job
+            modelBuilder.Entity<Job>()
+                .HasIndex(j => j.PostId)
+                .IsUnique();
+        }
     }
 }
